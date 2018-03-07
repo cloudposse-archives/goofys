c345ed085c48ab203bead1cff5404ebae04a2c94
aknysh <andriy.knysh@gmail.com>
2018-03-06 19:03:18 -0500

    Initial implementation

---
+++ rootfs/usr/bin/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+syslog-ng -f /etc/syslog-ng/syslog-ng.conf
+
+goofys -f --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE -o nonempty $BUCKET $MOUNT_DIR
