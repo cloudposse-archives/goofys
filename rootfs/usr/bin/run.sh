57c8db713d9e150b8617a0f37e7fe57f8b79c97d
Anwar Knyane <arsenic69@outlook.com>
2019-12-11 08:15:17 +0300

    added endpoint (#2)
    * Update run.sh
    
    added endpoint
    
    * Update Dockerfile
    
    Added endpoint

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -2,4 +2,4 @@
 
 syslog-ng -f /etc/syslog-ng/syslog-ng.conf
 
-goofys -f --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE -o nonempty $BUCKET $MOUNT_DIR
+goofys -f --endpoint $ENDPOINT --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE -o nonempty $BUCKET $MOUNT_DIR
