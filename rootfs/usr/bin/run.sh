4733fc3a7d799be0b8966f0ace84db83bdfba525
Antonio <antonio.maristany9205@gmail.com>
2019-12-25 20:40:42 -0400

    setting credentials file at starup

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -2,4 +2,10 @@
 
 syslog-ng -f /etc/syslog-ng/syslog-ng.conf
 
+cat <<- EOF >  ~/.aws/credentials
+   [default]
+   aws_access_key_id = $AWS_ACCESS_KEY
+   aws_secret_access_key = $AWS_SECRET_KEY
+EOF
+
 goofys -f --endpoint $ENDPOINT --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE -o nonempty $BUCKET $MOUNT_DIR
